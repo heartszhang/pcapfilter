619facb2a30339861ff528293cf54902bf69b189
hearts <hearts.zhang@gmail.com>
2014-09-13 18:51:38 +0800

    - a working version - need a modified gopcap

--- pcapsplit.go
+++ pcapsplit.go
@@ -4,20 +4,19 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/akrennmair/gopcap"
 	"os"
 	"time"
-
-	"github.com/akrennmair/gopcap"
 )
 
 var (
-	dest_ip    string
-	target_dir string
+	server_ip  string
 	input_file string
 )
 
 func init() {
-
+	flag.StringVar(&server_ip, "server-ip", "114.66.198.5", "114.66.198.5")
+	flag.StringVar(&input_file, "input", "target.cap", "pcap file")
 }
 
 type session_stat struct {
@@ -30,14 +29,19 @@ type session_stat struct {
 	sfin_tm     time.Time
 	rcvd_length int
 	sent_length int
-	req_length  uint32
-	rsp_length  uint32
+	req_length  int
+	rsp_length  int
 	key         string
+	sent_sn     uint32
+	rcvd_sn     uint32
+}
+
+func duration_milli(end, begin time.Time) int64 {
+	return int64(end.Sub(begin) / time.Millisecond)
 }
 
 const (
-	ss_reserved = 1 << iota
-	ss_syn_rcvd
+	ss_syn_rcvd uint16 = 1 << iota
 	ss_syn_sent
 	ss_push_rcvd
 	ss_push_sent
@@ -66,35 +70,48 @@ func split_pcap(input, svrip string) error {
 
 		rcvd := pkt.IP.DestAddr() == svrip
 		sent := pkt.IP.SrcAddr() == svrip
-		if rcvd == false || sent == false {
+		if rcvd == false && sent == false {
 			fmt.Println("discard", pkt.IP.DestAddr(), pkt.IP.SrcAddr())
 			continue
 		}
 		daddr := pkt.IP.DestAddr() + "-" + u16_to_string(pkt.TCP.DestPort)
 		saddr := pkt.IP.SrcAddr() + "-" + u16_to_string(pkt.TCP.SrcPort)
-		key := daddr + "_" + saddr
+		key := daddr + "-" + saddr
 		if sent {
-			key = saddr + "_" + daddr
+			key = saddr + "-" + daddr
 		}
 		sess := sns[key]
+
 		if rcvd == true && pkt.TCP.Flags&pcap.TCP_SYN != 0 && sess.flags&ss_syn_rcvd == 0 {
 			sess.id++
 			sess.flags = 0
 			sess.syn_tm = pkt.Time
 			sess.flags |= ss_syn_rcvd
+			sess.rcvd_sn = pkt.TCP.Seq
+			sess.sent_sn = 0
+			sess.rcvd_length = 0
+			sess.sent_length = 0
+			sess.key = key
 		}
 		if sent == true && pkt.TCP.Flags&pcap.TCP_SYN != 0 && sess.flags&ss_syn_sent == 0 {
 			sess.flags |= ss_syn_sent
+			sess.sent_sn = pkt.TCP.Seq
+		}
+		payload := pkt.IP.Len() - int(pkt.IP.Ihl*4) - int(pkt.TCP.DataOffset*4)
+		rseq := pkt.TCP.Seq - sess.rcvd_sn
+
+		if sent {
+			rseq = pkt.TCP.Seq - sess.sent_sn
 		}
 		if rcvd == true && pkt.TCP.Flags&pcap.TCP_PSH != 0 && (sess.flags&ss_push_rcvd) == 0 {
 			sess.flags |= ss_push_rcvd
 			sess.req_tm = pkt.Time
-			sess.req_length = pkt.Len - uint32(pkt.TCP.DataOffset)
+			sess.req_length = int(payload)
 		}
 		if sent == true && pkt.TCP.Flags&pcap.TCP_PSH != 0 && (sess.flags&ss_push_sent) == 0 {
 			sess.flags |= ss_push_sent
 			sess.rsp_tm = pkt.Time
-			sess.rsp_length = pkt.Len - uint32(pkt.TCP.DataOffset)
+			sess.rsp_length = int(payload)
 		}
 		if rcvd == true && pkt.TCP.Flags&pcap.TCP_FIN != 0 && sess.flags&ss_fin_rcvd == 0 {
 			sess.flags |= ss_fin_rcvd
@@ -104,25 +121,20 @@ func split_pcap(input, svrip string) error {
 			sess.flags |= ss_fin_sent
 			sess.sfin_tm = pkt.Time
 		}
-		payload := pkt.IP.Len() - int(pkt.IP.Ihl*4) - int(pkt.TCP.DataOffset*4)
 
-		if sent == true && int(sess.rcvd_length) < pkt.TCP.Ack {
-			sess.rcvd_length = pkt.TCP.Ack
+		if sent == true && sess.rcvd_length < int(rseq) {
+			sess.rcvd_length = int(rseq)
 		}
-		if rcvd == true && int(sess.sent_length) < pkt.TCP.Ack {
-			sess.sent_length = pkt.TCP.Ack
+		if rcvd == true && sess.sent_length < int(rseq) {
+			sess.sent_length = int(rseq)
 		}
-		/*
-					if rcvd == true && sess.rcvd_length < int(pkt.TCP.Seq)+payload {
-						sess.rcvd_length = pkt.TCP.Seq + payload
-					}
-			    			if sent == true && sess.sent_length < pkt.TCP.Seq+payload {
-							sess.sent_length = pkt.TCP.Seq + payload
-						}
-		*/
-		if is_session_ok(sess) == true {
+
+		if is_session_ok(sess) {
 			fmt.Println(sess)
+			sess.flags = 0
 		}
+		sns[key] = sess
+
 	}
 	return nil
 }
@@ -134,31 +146,31 @@ func is_session_ok(this session_stat) bool {
 
 func (this session_stat) String() string {
 	var (
-		d  = int64(this.sfin_tm.Sub(this.syn_tm) / time.Millisecond)
-		h  = int64(this.rsp_tm.Sub(this.req_tm) / time.Millisecond)
-		hs = int64(this.rfin_tm.Sub(this.rsp_tm) / time.Millisecond)
+		d  = duration_milli(this.sfin_tm, this.syn_tm)
+		h  = duration_milli(this.rsp_tm, this.req_tm)
+		hs = duration_milli(this.rfin_tm, this.req_tm)
 	)
-	return fmt.Sprintf(this.key, "id", this.id, "duration", d, "handle", h, "handle-sent", hs, "rcvd-len", this.rcvd_length, "sent-len", this.sent_length)
+	return fmt.Sprint("du ", d, " handle ", h, " handle-sent ", hs, " rcvd-len ", this.rcvd_length, " sent-len ", this.sent_length, " req-len ", this.req_length, " resp-len ", this.rsp_length, " ", this.key, " id ", this.id)
 }
 
 func main() {
 	flag.Parse()
-	if err := exist_file(input_file); err != nil {
-		panic(err)
+
+	if !is_exist_file(input_file) {
+		fmt.Errorf(input_file, "not exists")
+		return
 	}
-	if err := exist_directory(target_dir); err != nil {
-		panic(err)
-	}
-	err := split_pcap(input_file, target_dir)
+	err := split_pcap(input_file, server_ip)
 	fmt.Println(err)
 }
 
 func u16_to_string(v uint16) string {
 	return fmt.Sprint(v)
 }
+
 func is_exist_file(fp string) bool {
-	return false
-}
-func is_exist_directory(dir string) bool {
-	return false
+	if _, err := os.Stat(fp); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
